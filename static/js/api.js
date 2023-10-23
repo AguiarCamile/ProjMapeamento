330bdc69aeb715883c1b6ec468eb6866181ea653
Kewelyn <kewelyncout@gmail.com>
2023-10-23 15:28:16 -0300

    front-end (ainda tem coisa pra ajeitar)

--- static/js/api.js
+++ static/js/api.js
@@ -3,50 +3,65 @@ document.addEventListener('DOMContentLoaded', function() {
     const microrregiaoSelect = document.getElementById('microrregiao');
     const cidadeSelect = document.getElementById('cidade');
 
+    // Função para inicializar o Select2 em um campo de seleção
+    function initSelect2(selectElement) {
+        $(selectElement).select2({
+            width: '100%', // Defina a largura conforme necessário
+            placeholder: 'Selecione uma opção',
+            allowClear: true, // Opcional: permite que o usuário desmarque a seleção
+        });
+    }
 
+    // Inicialize o Select2 para cada campo de seleção
+    initSelect2(estadoSelect);
+    initSelect2(microrregiaoSelect);
+    initSelect2(cidadeSelect);
+
+    // Busque os dados dos estados da API e adicione-os ao campo de Estado
     fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
         .then(response => response.json())
         .then(data => {
             data.forEach(item => {
-                const option = document.createElement('option');
-                option.value = item.nome;
-                option.textContent = item.nome;
+                const option = new Option(item.nome, item.nome, false, false);
                 estadoSelect.appendChild(option);
             });
 
             estadoSelect.value = "{{ db.Estado }}";
+
+            // Atualize o Select2 após adicionar as opções
+            $(estadoSelect).trigger('change');
         })
         .catch(error => console.error('Erro ao buscar dados do estado:', error));
 
-
+    // Busque os dados da microrregião da API e adicione-os ao campo de Microrregião
     fetch('https://servicodados.ibge.gov.br/api/v1/localidades/microrregioes?orderBy=nome')
         .then(response => response.json())
         .then(data => {
             data.forEach(item => {
-                const option = document.createElement('option');
-                option.value = item.nome;
-                option.textContent = item.nome;
+                const option = new Option(item.nome, item.nome, false, false);
                 microrregiaoSelect.appendChild(option);
             });
 
             microrregiaoSelect.value = "{{ db.Microrregiao }}";
+
+            // Atualize o Select2 após adicionar as opções
+            $(microrregiaoSelect).trigger('change');
         })
-        .catch(error => console.error('Erro ao buscar dados da microrregiao:', error));
+        .catch(error => console.error('Erro ao buscar dados da microrregião:', error));
 
-
+    // Busque os dados da cidade da API e adicione-os ao campo de Cidade
     fetch('https://servicodados.ibge.gov.br/api/v1/localidades/municipios?orderBy=nome')
         .then(response => response.json())
         .then(data => {
             data.forEach(item => {
-                const option = document.createElement('option');
-                option.value = item.nome;
-                option.textContent = item.nome;
+                const option = new Option(item.nome, item.nome, false, false);
                 cidadeSelect.appendChild(option);
             });
 
             cidadeSelect.value = "{{ db.Cidade }}";
+
+            // Atualize o Select2 após adicionar as opções
+            $(cidadeSelect).trigger('change');
         })
         .catch(error => console.error('Erro ao buscar dados da cidade:', error));
 });
-
-
